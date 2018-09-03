4eb61a2c1e146add826c8f0a70606ede5f0f583f
Uros Stojiljkovic <urosss007@gmail.com>
2018-09-03 17:34:24 +0200

    Septembar 2018 Z2 ER

--- 2018/Septembar 2018 Z2/main.c
+++ 2018/Septembar 2018 Z2/main.c
@@ -1,15 +1,15 @@
 ﻿/*
-	Napisati program na programskom jeziku C koji obrađuje stringove. Program na početku učita string koji sadrži
-	proizvoljan broj pojava para znakova %s. Zatim program učitava stringove koje ubacuje u prvi string. Za ubacivanje
-	jednog stringa neophodno je realizovati i koristiti funkciju char* insertString(char *s1, char *s2);. Funkcija
-	treba da ubaci string s2 u string s1, tako što zameni prvu pojavu para znakova %s u stringu s1 stringom s2. Stringovi za
-	ubacivanje se učitavaju sa standardnog ulaza, svaki string u zasebnom redu. Učitavanje i ubacivanje stringova se vrši sve
-	dok postoji bar jedan par znakova %s. Potrebno je napisati i koristiti funkciju koja pronalazi poziciju prvog pojavljivanja
-	%s u stringu int find(char *s);. Funkcija vraća negativan broj ako ne postoji %s u stringu. Program na kraju treba
-	da ispiše novoformirani string. Maksimalna dužina svakog stringa je 100, ali je potrebno koristiti samo onoliko
-	memorijskog prostora koliko je potrebno, drugim rečima prilikom svakog ubacivanja memorijski prostor za string je
-	potrebno proširiti za tačno onoliko koliko je potrebno da ceo sadržaj stane. Voditi računa o ispravnom korišćenju
-	dinamičke memorije.
+Napisati program na programskom jeziku C koji obrađuje stringove. Program na početku učita string koji sadrži
+proizvoljan broj pojava para znakova %s. Zatim program učitava stringove koje ubacuje u prvi string. Za ubacivanje
+jednog stringa neophodno je realizovati i koristiti funkciju char* insertString(char *s1, char *s2);. Funkcija
+treba da ubaci string s2 u string s1, tako što zameni prvu pojavu para znakova %s u stringu s1 stringom s2. Stringovi za
+ubacivanje se učitavaju sa standardnog ulaza, svaki string u zasebnom redu. Učitavanje i ubacivanje stringova se vrši sve
+dok postoji bar jedan par znakova %s. Potrebno je napisati i koristiti funkciju koja pronalazi poziciju prvog pojavljivanja
+%s u stringu int find(char *s);. Funkcija vraća negativan broj ako ne postoji %s u stringu. Program na kraju treba
+da ispiše novoformirani string. Maksimalna dužina svakog stringa je 100, ali je potrebno koristiti samo onoliko
+memorijskog prostora koliko je potrebno, drugim rečima prilikom svakog ubacivanja memorijski prostor za string je
+potrebno proširiti za tačno onoliko koliko je potrebno da ceo sadržaj stane. Voditi računa o ispravnom korišćenju
+dinamičke memorije.
 */
 
 #include <stdio.h>
